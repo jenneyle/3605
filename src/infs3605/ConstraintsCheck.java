60ea7c0f0ec1c5fb9edc0cce892b1fb068c63016
shilan-m <z5211159@ad.unsw.edu.au>
2020-03-28 23:19:39 +1100

    Update ConstraintsCheck.java

--- src/infs3605/ConstraintsCheck.java
+++ src/infs3605/ConstraintsCheck.java
@@ -32,11 +32,9 @@ public class ConstraintsCheck {
                 + "inner join Staff on Staff.staff_id=Allocation.staff_id\n"
                 + "where Staff.staff_id='" + staffid + "'\n"
                 + "GROUP by Staff.staff_id";
-        System.out.println(searchQuery);
         String allocateweightQ = "SELECT (weighting_term*0.002+face_time*0.1+prep_dev*0.1) as weight FROM Allocation\n"
                 + "inner join Weighting on Allocation.course_id=Weighting.course_id and allocation_year=year and allocation_term=term\n"
                 + "where Weighting.course_id='" + courseid + "'and year=" + year + " and term='" + term + "'";
-        System.out.println(allocateweightQ);
         try {
             ResultSet rs1 = database.getResultSet(searchQuery);
             while(rs1.next()){
