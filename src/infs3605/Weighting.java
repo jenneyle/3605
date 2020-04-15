8b2e1726641533aac71925e5d74f6ba9220de625
Jenney Le <jenneyle@192-168-1-105.tpgi.com.au>
2020-04-15 11:13:06 +1000

    weighting detail button half displays

--- src/infs3605/Weighting.java
+++ src/infs3605/Weighting.java
@@ -27,8 +27,19 @@ public class Weighting {
     private IntegerProperty facetoface_hours;
     private IntegerProperty pd_hours;
     private DoubleProperty weighting_term;
+    private IntegerProperty tutorial_hrs;
+    private IntegerProperty lecture_hrs;
+    private IntegerProperty consultation_hrs;
+    private IntegerProperty marking_hrs;
+    private IntegerProperty tutorial_prep;
+    private IntegerProperty lecture_prep;
+    private IntegerProperty staff_development;
+    
     
     private Button detailsButton;
+
+    public Weighting() {
+    }
     
 
     public Weighting(int weight_id, String course_id, int year, String term, int students_enrolled, int facetoface_hours,int pd_hours, double weighting_term){
@@ -45,6 +56,24 @@ public class Weighting {
         //this.editButton = new Button("Edit");
         this.detailsButton = new Button("Details");
     }
+
+    public Weighting(int weight_id, String course_id, int year, String term, int students_enrolled, double weighting_term, int tutorial_hrs, int lecture_hrs, int consultation_hrs, int marking_hrs, int tutorial_prep, int lecture_prep, int staff_development) {
+        this.weight_id = new SimpleIntegerProperty(weight_id);
+        this.course_id = new SimpleStringProperty(course_id);
+        this.year = new SimpleIntegerProperty(year);
+        this.term = new SimpleStringProperty(term);
+        this.students_enrolled = new SimpleIntegerProperty(students_enrolled);
+        this.weighting_term = new SimpleDoubleProperty(weighting_term);
+        this.tutorial_hrs =new SimpleIntegerProperty(tutorial_hrs);
+        this.lecture_hrs = new SimpleIntegerProperty(lecture_hrs);
+        this.consultation_hrs = new SimpleIntegerProperty(consultation_hrs);
+        this.marking_hrs = new SimpleIntegerProperty(marking_hrs);
+        this.tutorial_prep = new SimpleIntegerProperty(tutorial_prep);
+        this.lecture_prep = new SimpleIntegerProperty(lecture_prep);
+        this.staff_development = new SimpleIntegerProperty(staff_development);
+    }
+    
+    
 
     public Button getWDetailsButton() {
         return detailsButton;
@@ -116,6 +145,53 @@ public class Weighting {
 
     public void setWeighting_term(double weighting_term) {
         this.weighting_term.set(weighting_term);
+    }
+    public int getTutorial_hrs() {
+        return tutorial_hrs.get();
+    }
+    public void setTutorial_hrs(int tutorial_hrs) {
+        this.tutorial_hrs.set(tutorial_hrs);
+    }
+    public int getLecture_hrs() {
+        return lecture_hrs.get();
+    }
+
+    public void setLecture_hrs(int lecture_hrs) {
+        this.lecture_hrs.set(lecture_hrs);
+    }
+    
+    public int getConsultation_hrs() {
+        return consultation_hrs.get();
+    }
+
+    public void setConsultation_hrs(int consultation_hrs) {
+        this.consultation_hrs.set(consultation_hrs);
+    }
+    public int getMarking_hrs() {
+        return marking_hrs.get();
+    }
+    public void setMarking_hrs(int marking_hrs) {
+        this.marking_hrs.set(marking_hrs);
+    }
+    public int getTutorial_prep() {
+        return tutorial_prep.get();
+    }
+    public void setTutorial_prep(int tutorial_prep) {
+        this.tutorial_prep.set(tutorial_prep);
+    }
+    public int getLecture_prep() {
+        return lecture_prep.get();
+    }
+
+    public void setLecture_prep(int lecture_prep) {
+        this.lecture_prep.set(lecture_prep);
+    }
+    public int getStaff_development() {
+        return staff_development.get();
+    }
+
+    public void setStaff_development(int staff_development) {
+        this.staff_development.set(staff_development);
     }
 
 }
--- src/infs3605/WeightingDetailsController.java
+++ src/infs3605/WeightingDetailsController.java
@@ -54,21 +54,20 @@ public class WeightingDetailsController {
 
         try {
             Database.openConnection();
-            System.out.println("pls work");
-            ResultSet rs = conn.createStatement().executeQuery("SELECT weight_id, course_id, Year, Term, students_enrolled, tutorial_hrs, lecture_hrs, consultation_hrs, marking_hrs, tutorial_prep, lecture_prep, staff_development, weighting_term, FROM Weighting WHERE weight_id" + iWeightingId);
+            ResultSet rs = conn.createStatement().executeQuery("SELECT weight_id, course_id, Year, Term, students_enrolled, tutorial_hrs, lecture_hrs, consultation_hrs, marking_hrs, tutorial_prep, lecture_prep, staff_development, weighting_term FROM Weighting WHERE weight_id =" + iWeightingId);
 
             courseCode.setText(rs.getString(2));
-            term.setText(rs.getString(4));
-            year.setText(rs.getString(3));
-            studentsEnrolled.setText(rs.getString(5));
-            tutorialHrs.setText(rs.getString(6));
-            lectureHrs.setText(rs.getString(7));
-            consultationHrs.setText(rs.getString(8));
-            markingHrs.setText(rs.getString(9));
-            tutorialPrep.setText(rs.getString(10));
-            lecturePrep.setText(rs.getString(11));
-            staffDev.setText(rs.getString(12));
-            totalWeighting.setText(rs.getString(13));
+            term.setText("Term: "+rs.getString(4));
+            year.setText("Year: " +rs.getString(3));
+            studentsEnrolled.setText("Students Enrolled: " +rs.getString(5));
+            tutorialHrs.setText("Tutorial Hours: " +rs.getString(6));
+            lectureHrs.setText("Lecture Hours: "+rs.getString(7));
+            consultationHrs.setText("Consultation Hours: "+rs.getString(8));
+            markingHrs.setText("Marking Hours: "+rs.getString(9));
+            tutorialPrep.setText("Tutorial Preparation: "+rs.getString(10));
+            lecturePrep.setText("Lecture Preparation: "+rs.getString(11));
+            staffDev.setText("Staff Development Hours: " +rs.getString(12));
+            totalWeighting.setText("Total Weighting: "+rs.getString(13));
 
 //            if(rs.getInt(6)==1){
 //                lic.setText("LIC: Yes");
@@ -77,7 +76,7 @@ public class WeightingDetailsController {
 //            }
 //            courseName.setText(rs.getString(7));
 //            staffName.setText(rs.getString(8) + " " + rs.getString(9));
-            conn.close();
+            //conn.close();
 
         } catch (SQLException ex) {
             ex.printStackTrace();
